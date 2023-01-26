8379e92e643340ad8a44dc03056a27ca99b4921b
Laraib <theyoungcoder24@gmail.com>
2023-01-27 00:26:21 +0530

    new

--- App.js
+++ App.js
@@ -1,1 +1,9 @@
-
+console.log("hello");
+var savedBtn = document.getElementById('savedBtn');
+var searchBtn = document.getElementById('searchBtn');
+function SavedButton(){
+    console.log("working save btn")
+}
+function SearchButton(){
+    console.log("working search btn")
+}
