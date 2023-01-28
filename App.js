f0197ec60886f1430518caad14ad66cd5f3310c1
Laraib <theyoungcoder24@gmail.com>
2023-01-28 11:44:33 +0530

    new

--- App.js
+++ App.js
@@ -1,9 +1,29 @@
 console.log("hello");
-var savedBtn = document.getElementById('savedBtn');
-var searchBtn = document.getElementById('searchBtn');
+const savedBtn = document.getElementById('savedBtn');
+///const searchBtn = document.getElementById('searchBtn');
+
 function SavedButton(){
     console.log("working save btn")
 }
+
 function SearchButton(){
+    //event.preventDefault();
+
     console.log("working search btn")
+   var problem = document.getElementById('problemBar').value;
+   var category = document.getElementById('categoryBar').value;
+   console.log(problem);
+   console.log(category);
+
+   var input = document.querySelector('container-input inputBox1');
+   input.textContent = problem;
+
+//    document.getElementsByClassName("inputBox1").textContent = problem;
+//    document.getElementsByClassName("inputBox2").innerHTML = category;
+
+}
+
+
+function delBtn(){
+    console.log("working delete btn")
 }
